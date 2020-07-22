6494ec8b13070a9394286fb7434fca3fa29c8e28
Harsha Maiya <harshamaiya99@gmail.com>
2020-07-22 11:32:15 +0530

    commmited by HM

--- app/src/main/java/com/example/sih2/FeedbackFragment.java
+++ app/src/main/java/com/example/sih2/FeedbackFragment.java
@@ -1,5 +1,6 @@
 package com.example.sih2;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,18 +10,48 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class FeedbackFragment extends Fragment {
 
     SharedPrefrencesHelper sharedPrefrencesHelper;
 
+    EditText editTextSubject,editTextMessage;
+    Button send;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_feedback,container,false);
-        sharedPrefrencesHelper =new SharedPrefrencesHelper(FeedbackFragment.this.getActivity());
-        // write the code for about here
 
+        super.onCreate(savedInstanceState);
+        //(R.layout.fragment_feedback);
+
+        editTextSubject=view.findViewById(R.id.edit_text_subject);
+        editTextMessage=view.findViewById(R.id.edit_text_message);
+        send=view.findViewById(R.id.button_send);
+
+        send.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View arg0) {
+                String to="maiya.harsha03@gmail.com";
+                String subject=editTextSubject.getText().toString();
+                String message=editTextMessage.getText().toString();
+
+                Intent email = new Intent(Intent.ACTION_SEND);
+                email.putExtra(Intent.EXTRA_EMAIL, new String[]{ to});
+                email.putExtra(Intent.EXTRA_SUBJECT, subject);
+                email.putExtra(Intent.EXTRA_TEXT, message);
+
+                //need this to prompts email client only
+                email.setType("message/rfc822");
+
+                startActivity(Intent.createChooser(email, "Choose an Email client :"));
+            }
+        });
         return view;
     }
+
 }
