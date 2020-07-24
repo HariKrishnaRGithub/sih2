6f63f5312aed09087cc768978752f57ba97ebf5a
User <chinthanshetty100@gmail.com>
2020-07-24 16:29:24 +0530

    imageslider

--- app/src/main/java/com/example/sih2/EmpHomeFragment.java
+++ app/src/main/java/com/example/sih2/EmpHomeFragment.java
@@ -15,10 +15,16 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.models.SlideModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EmpHomeFragment extends Fragment{
     employeeHomeSelected listener;
     SharedPrefrencesHelper sharedPrefrencesHelper;
+
 
 
     @Nullable
@@ -27,6 +33,15 @@ public class EmpHomeFragment extends Fragment{
         View view=inflater.inflate(R.layout.fragment_emp_home,container,false);
 
         sharedPrefrencesHelper =new SharedPrefrencesHelper(this.getActivity());
+        ImageSlider imageSlider=view.findViewById(R.id.slider);
+
+        List<SlideModel> slideModels=new ArrayList<>();
+        slideModels.add(new SlideModel("https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__340.jpg","Google"));
+        slideModels.add(new SlideModel("https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__340.jpg","Microsoft"));
+        slideModels.add(new SlideModel("https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__340.jpg","Apple"));
+        slideModels.add(new SlideModel("https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__340.jpg","WhatsApp"));
+        slideModels.add(new SlideModel("https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__340.jpg","Infosys"));
+        imageSlider.setImageList(slideModels,true);
 
 
 
