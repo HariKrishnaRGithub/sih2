30a321416b51a41d4d37564337995083f87bed60
Harsha Maiya <harshamaiya99@gmail.com>
2020-07-27 21:21:23 +0530

    Commited by Harsha Maiya

--- app/src/main/java/com/example/sih2/SettingsFragment.java
+++ app/src/main/java/com/example/sih2/SettingsFragment.java
@@ -19,7 +19,7 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_settings,container,false);
         sharedPrefrencesHelper =new SharedPrefrencesHelper(SettingsFragment.this.getActivity());
-        // write the code for about here
+
 
         return view;
     }
