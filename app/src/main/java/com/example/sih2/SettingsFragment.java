4988a0911fa65aef4d8fc3cde51e3f054fda4196
Harsha Maiya <harshamaiya99@gmail.com>
2020-08-02 23:02:10 +0530

    Commited by Harsha Maiya

--- app/src/main/java/com/example/sih2/SettingsFragment.java
+++ app/src/main/java/com/example/sih2/SettingsFragment.java
@@ -19,56 +19,13 @@ public class SettingsFragment extends Fragment {
 
     SharedPrefrencesHelper sharedPrefrencesHelper;
 
-    private Switch aSwitch;
-
-    public static final String MyPREFERENCES="nightModePrefs";
-    public static final String KEY_ISNIGHTMODE="isNightMode";
-    SharedPreferences sharedPreferences;
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_settings,container,false);
         sharedPrefrencesHelper =new SharedPrefrencesHelper(SettingsFragment.this.getActivity());
-        super.onCreate(savedInstanceState);
-
-        sharedPreferences=getActivity().getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
-
-        aSwitch=view.findViewById(R.id.switch_darkmode);
-
-        checkNightModeActivated();
-
-        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                    saveNightModeState(true);
-                    getActivity().recreate();
-                } else {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-                    saveNightModeState(false);
-                    getActivity().recreate();
-                }
-            }
-        });
+        // write the code for about here
 
         return view;
-    }
-    private void saveNightModeState(boolean nightmode) {
-        SharedPreferences.Editor editor=sharedPreferences.edit();
-        editor.putBoolean(KEY_ISNIGHTMODE,nightmode);
-        editor.apply();
-    }
-
-    public void checkNightModeActivated(){
-        if(sharedPreferences.getBoolean(KEY_ISNIGHTMODE,false)){
-            aSwitch.setChecked(true);
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-        }
-        else{
-            aSwitch.setChecked(false);
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-        }
     }
 }
